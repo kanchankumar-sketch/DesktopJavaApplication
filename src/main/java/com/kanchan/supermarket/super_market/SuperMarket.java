8166e4000465807c700acc60aeb29f53a8ed855d
Kanchan kumar Sharma <70506833+kanchankumar-sketch@users.noreply.github.com>
2021-11-05 17:47:56 +0530

    application desktop

---
+++ src/main/java/com/kanchan/supermarket/super_market/SuperMarket.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.kanchan.supermarket.super_market;
+
+/**
+ *
+ * @author kanch
+ */
+public class SuperMarket {
+    public static void main(String[] args) {
+        
+    }
+    
+}
